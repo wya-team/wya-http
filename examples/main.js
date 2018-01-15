22e7c86953d0dc2825392a9b771f30dcf4fa632b
Deot <zrd0921@qq.com>
2018-01-15 21:21:42 +0800

    add version
    XMLHttpRequest 取代 fetch

--- examples/main.js
+++ examples/main.js
@@ -1,2 +1,13 @@
-import * as utils from '../src/main';
-console.log(utils);
+import { ajax } from '../src/main';
+const request = ajax({
+	url: `http://localhost:3000/api/test`
+}).then((res) => {
+	console.log(res, 0);
+}).catch((res) => {
+	console.log(res);
+});
+setTimeout(() => {
+	request.cancel();
+}, Math.random() * 700);
+
+
