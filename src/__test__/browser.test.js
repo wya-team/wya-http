649a8587d7c1466957cefdf317b064edab6d4c7f
Mr.Z <zrd0921@qq.com>
2019-03-24 18:13:49 +0800

    update:test

--- src/__test__/browser.test.js
+++ src/__test__/browser.test.js
@@ -1,27 +1,71 @@
 import createHttpClient from '..';
+import { ERROR_CODE } from '../core/HttpError';
 describe('browser.js', () => {
-	test('无URL验证错误', () => {
-		let $ = createHttpClient();
-		expect(typeof $.ajax).toBe('function');
+	let $ = createHttpClient();
+	expect(typeof $.ajax).toBe('function');
 
-		$.ajax({
-
-		}).then((res) => {
-			console.log(res);
-		}).catch((e) => {
-			console.log(e);
-		});
+	test('无URL验证错误', async () => {
+		try {
+			let response = await $.ajax();
+		} catch (e) {
+			expect(e.code).toBe(ERROR_CODE.HTTP_URL_EMPTY);
+		}
 	});
 
-	test('验证api', () => {
-		let $ = createHttpClient();
-
-		$.ajax({
-			url: 'https://oa2.ruishan666.com/_sale/sale/statistics-customer/first-page-statistics.json'
-		}).then((res) => {
-			console.log(res);
-		}).catch((e) => {
-			console.log(e);
-		});
+	test('localData验证: status = 1', async () => {
+		let options = {
+			localData: {
+				status: 1,
+				data: {
+					user: 'wya'
+				}
+			}
+		};
+		let response = await $.ajax(options);
+		expect(response.data.user).toBe('wya');
 	});
+
+	test('localData验证: status = 0', async () => {
+		try {
+			let options = {
+				localData: {
+					status: 0,
+					data: {
+						user: 'wya'
+					}
+				}
+			};
+			await $.ajax(options);
+		} catch (response) {
+			expect(response.data.user).toBe('wya');
+		}
+	});
+
+	test('localData验证: status = null', async () => {
+		try {
+			let options = {
+				localData: {
+					status: null,
+					data: {
+						user: 'wya'
+					}
+				}
+			};
+			await $.ajax(options);
+		} catch (response) {
+			expect(response.data.user).toBe('wya');
+		}
+	});
+
+	// 存在问题
+	// test('验证网络请求', async () => {
+	// 	try {
+	// 		let options = {
+	// 			url: 'http://api.github.com/users/wya-team',
+	// 		};
+	// 		await $.ajax(options);
+	// 	} catch (response) {
+	// 		expect(response).toBe('wya');
+	// 	}
+	// });
 });
